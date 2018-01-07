317fb680707799644b59c7c3d2fa2e1a8280fabe
kozak94 <Muhaylo.Gandjyk@gmail.com>
2018-01-07 13:40:38 +0200

    added lesson 6 task1

---
+++ lesson 6/task1/scripts/file.js
@@ -0,0 +1,36 @@
+window.onload = function () {
+    var e1 = document.getElementById("first");
+    var e2 = document.getElementById("second");
+    var e3 = document.getElementById("third");
+
+    var p1 = document.getElementById("p1");
+    var p2 = document.getElementById("p2");
+    var p3 = document.getElementById("p3");
+
+    e1.addEventListener("click",
+        // e - параметр хранит объект с информацией о событии.
+        function(e) {
+            
+            p1.style.backgroundColor = "red";
+            alert("Обработчик p1");
+            // прекращение дальнейшего маршрута события.
+            e.stopPropagation();
+        },
+    true);
+
+    e2.addEventListener("click",
+        function (e) {
+            p2.style.backgroundColor = "green";
+            alert("Обработчик p2");
+            e.stopPropagation();
+        },
+    false);
+
+    e3.addEventListener("click",
+        function (e) {
+            p3.style.backgroundColor = "blue";
+            alert("Обработчик p3");
+
+        },
+    false);
+}
